e85ed1dad18646dd37efe1a626518150a24779b7
Nicolas Frankel <nicolas@frankel.ch>
2019-02-19 10:31:55 +0100

    Migrate to reactive all the way down

--- src/main/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplication.kt
+++ src/main/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplication.kt
@@ -1,14 +1,29 @@
 package ch.frankel.blog.springfu.migrationdemo
 
-import org.springframework.boot.*
-import org.springframework.boot.autoconfigure.*
-import org.springframework.data.repository.*
+import io.r2dbc.h2.H2ConnectionConfiguration
+import io.r2dbc.h2.H2ConnectionFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.annotation.Id
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDate
-import javax.persistence.*
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 class MigrationDemoApplication
+
+@Configuration
+class DataConfiguration : AbstractR2dbcConfiguration() {
+
+    override fun connectionFactory() = H2ConnectionFactory(H2ConnectionConfiguration.builder()
+        .inMemory("testdb")
+        .username("sa")
+        .build())
+}
 
 fun main(args: Array<String>) {
     runApplication<MigrationDemoApplication>(*args)
@@ -24,7 +39,6 @@ class PersonController(private val personRepository: PersonRepository) {
     fun readOne(@PathVariable id: Long) = personRepository.findById(id)
 }
 
-@Entity
 class Person(@Id val id: Long, val firstName: String, val lastName: String, val birthdate: LocalDate? = null)
 
-interface PersonRepository : PagingAndSortingRepository<Person, Long>
+interface PersonRepository : ReactiveCrudRepository<Person, Long>
